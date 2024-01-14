bcc93b7fae2b6fbbf9641eb1b2d43c3c6a3a9ceb
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-01-14 15:59:06 +0100

    Move "filterWeatherDetails()" to the utils.mjs file"

--- src/util.mjs
+++ src/util.mjs
@@ -6,8 +6,42 @@ export function getCityNameFromUserInput() {
 
 export async function loadInitialWeatherData() {
   const cityName = 'lobito';
-  const weather = await getCurrentWeather(cityName);
-  displayWeather(weather);
+  const weatherInfo = filterWeatherDetails(await getCurrentWeather(cityName));
+  displayWeather(weatherInfo);
+}
+function filterWeatherDetails(weatherInfo) {
+  return {
+    current: {
+      temperature: {
+        real: {
+          degreesCelsius: weatherInfo.current.temp_c,
+          degreesFahrenheit: weatherInfo.current.temp_f,
+        },
+        feelsLike: {
+          degreesCelsius: weatherInfo.current.condition.feelsLike_c,
+          degreesFahrenheit: weatherInfo.current.condition.feelsLike_f,
+        },
+      },
+      visibility: {
+        kilometers: weatherInfo.current.condition.vis_km,
+        miles: weatherInfo.current.condition.vis_miles,
+      },
+      wind: {
+        kilometersPerHour: weatherInfo.current.condition.wind_kph,
+        milesPerHour: weatherInfo.current.condition.wind_mph,
+      },
+      condition: weatherInfo.current.condition.text,
+      humidity: weatherInfo.current.condition.humidity,
+      uv: weatherInfo.current.condition.uv,
+    },
+    location: {
+      city: weatherInfo.location.name,
+      region: weatherInfo.location.region,
+      country: weatherInfo.location.country,
+      localTime: weatherInfo.location.localtime,
+      timeZone: weatherInfo.location.tz_id,
+    },
+  };
 }
 
 function displayWeather(weatherInfo) {
